61ccede917e4676d0e552aeff7e26f5a90abd589
AnCenergy <vinod@ancenergytech.com>
2021-08-20 22:24:16 +0530

    End of Section 2 ( 1 Commit  from Training)

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }  // Name is crucial for entity framework to  understand that this is going to be pk
+        public string UserName { get; set; } // stick with Capital "N" for Name as ASPnet core identity, that needs it to be this wya, so refactoring is less
+
+        // To explicitiy set the property, propfull can be used
+    }
+}
